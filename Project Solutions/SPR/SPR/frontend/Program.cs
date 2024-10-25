874485935f6ff2018858868a3cb014e7f2122638
Ranur-react <rahmatnur845@gmail.com>
2024-10-25 18:11:21 +0700

    git push

--- Project Solutions/SPR/SPR/frontend/Program.cs
+++ Project Solutions/SPR/SPR/frontend/Program.cs
@@ -32,11 +32,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-else
-{
-    app.Urls.Add("http://*:4002"); // Ensure the application listens on the correct port
 
-}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
