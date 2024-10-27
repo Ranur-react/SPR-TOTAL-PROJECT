7deb75879f83b79072a3409e94245f219bc45fdd
Ranur-react <rahmatnur845@gmail.com>
2024-10-27 19:52:40 +0700

    test deploy again

--- Project Solutions/SPR/SPR/frontend/Program.cs
+++ Project Solutions/SPR/SPR/frontend/Program.cs
@@ -2,6 +2,8 @@ using frontend.Base;
 using frontend.Repository.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://*:4002");
+builder.WebHost.UseUrls("https://*:4002");
 var services = builder.Services;
 services.AddScoped<Address>();
 services.AddScoped<SPRRepository>();
